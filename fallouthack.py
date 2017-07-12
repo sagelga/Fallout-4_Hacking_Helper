3e93e64ab32fab3d319002b3f1087eca574c1cf9
Kunanon Srisuntiroj <son9912@gmail.com>
2017-07-12 16:05:35 +0700

    Add original recommendation systems

--- fallouthack.py
+++ fallouthack.py
@@ -1,82 +1,61 @@
+def recommends(list_word):
+    lenght = len(list_word[0])
+    chars = []
+    for i in range(lenght):
+        s= "".join(v[i] for v in list_word)
+        char = max((s.count(j),j)for j in s)
+        chars.append(char)
+        
+    max_char = max(chars)
+    print("\nRecommends words :"+" ".join(i for i in list_word if i[chars.index(max_char)]==max_char[1]))
+
 def main():
     """This will call other functions and served as a caller"""
-    count = 1
-    results, tries = [], []
-    while 1:
-        text = input("Getting possible password #%d : "%count)
-        if "/" not in text:
-            results.append(text)
+    count, triggers = 1, 1
+    results = []
+    while triggers:
+        text = input("Getting possible password #%-2d : "%count)
+        if not text.startswith("/"):
+            results.append(text.upper())   
             count += 1
         else:
-            break
-
-    print(result_printer(results))
+            triggers = 0
+        
+    print("\nHere's what you have included.")
 
     while 1:
-        text = input("Please try some word \n What word have you tried? : ")
-        correctness = input("and they are what likeness? : ")
-
-        raw_text = []
-        raw_text.append(text)
-        raw_text.append(correctness)
-        tries.append(raw_text)
-
-        if correctness == 0:
-            zero_likeness(results, tries, text)
-        elif correctness == len(results[0]):
-            result_printer(results)
+        if len(results) <= 1:
+            print("You have solved the hack!")
+            print("The answer is :", results[0])
             break
-        elif correctness > 0:
-            likeness(results, tries, text)
-
+        
         result_printer(results)
+        recommends(results)
+        text = input("\n\nPlease try some word...\nWhat word have you tried? : ").upper()
+        correctness = int(input("and they are what likeness? : "))
+        print()
+        results = list_filter_word(results, text, correctness)
 
-
-
-
-def zero_likeness(results, tries, answer):
-    """This function is for the entropy that gives 0 likeness.
-
-        Program will delete the answer that are similar to the answer
-        and delete itself from existence"""
-
-    tries.append(answer)
-
-    return results, tries
-
-def likeness(results, tries, answer):
-    """This function is for entropy that gives at least 1 likeness.
-
-    Program will find the intersection of the results.
-    If it can detects that they are in pattern,
-    program will delete the word that are not in that pattern"""
-
-    return results, tries
+def list_filter_word(results, word, number):
+    possible_answer=[]
+    # print("----- Word -----|-- Similarity --")
+    for check_answer in results:
+        n = 0
+        for i,v in enumerate(word):
+            if check_answer[i] == v:
+                n += 1
+        # print(check_answer, n)
+        if n == number:
+            possible_answer.append(check_answer)
+    # print(possible_answer)
+    return possible_answer
 
 def result_printer(results):
     """Designing the way that the possible answer will be print out"""
 
     print("Possible answer is...")
+    print("---------------------")
     for i in results:
         print(i, end="\t")
 
 main()
-
-"""
-
-empty
-enact
-swore
-helps
-knows
-names
-stake
-marks
-large
-price
-unite
-stark
-torch
-handy
-
-"""
