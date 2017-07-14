e1c9764a37cca1aeb6452a86de846298c1ccd8b3
Kumamon Kuma <son9912@gmail.com>
2017-07-14 16:09:16 +0000

    Add more interface to terminals

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Fallout-4_Hacking_Helper
-Python language program that can understands and help you to hack Fallout 4 terminal easily
+# Fallout 4 Hacking Minigame Helper
+![](https://vignette2.wikia.nocookie.net/fallout/images/e/ec/Fo4_Hacker.png/revision/latest?cb=20170320162306)
+Program that helps you through the unsolvable riddles from the text.
+
+# Inspiration
+What inspires me is that when I played Fallout 4 game, the riddles is long and hard to hack by yourself <br>
+So, I create python program to find the answer to the riddles
+
+# How to use
+1. Start typing up all the possible answers (all the possible answer)
+2. Press enter continue
+3. Program will start finding relationships to the wording, and showing you the most possible answers
+4. Choose one, and tell the program that you chose what word, and ends up with how much likeness
+5. Repeat until you can find the answer
+
+During test run of the program, sample size of the word is 6 characters long (Advanced level Terminal) and they can hack it within 4 attempts
+
+I hoped you like the project!
+
+-- Crafted with love by son9912
--- fallouthack.py
+++ fallouthack.py
@@ -1,61 +1,123 @@
-def recommends(list_word):
-    lenght = len(list_word[0])
-    chars = []
-    for i in range(lenght):
-        s= "".join(v[i] for v in list_word)
-        char = max((s.count(j),j)for j in s)
-        chars.append(char)
-        
-    max_char = max(chars)
-    print("\nRecommends words :"+" ".join(i for i in list_word if i[chars.index(max_char)]==max_char[1]))
+import math
+import os
+import tkinter
 
 def main():
-    """This will call other functions and served as a caller"""
-    count, triggers = 1, 1
-    results = []
-    while triggers:
-        text = input("Getting possible password #%-2d : "%count)
-        if not text.startswith("/"):
-            results.append(text.upper())   
+    """Served as function caller and receptions"""
+    count, results = 1, []
+
+    """Input all the password"""
+    while 1:
+        screen_clear()
+        result_printer(results, "Vocabulary Lists")
+        text = input("Please type in possible password #%-2d : "%count)
+
+        if text.isalpha():
+            if count != 1:
+                if len(text) != len(results[0]):
+                    continue
+
+            results.append(text.upper())
             count += 1
+        elif text != "":
+            command_center(results)
         else:
-            triggers = 0
-        
-    print("\nHere's what you have included.")
+            screen_clear()
+            break
+
+    print("Done recieving more vocabulary...\n")
 
     while 1:
         if len(results) <= 1:
-            print("You have solved the hack!")
+            print("You have solved the riddle!")
             print("The answer is :", results[0])
             break
-        
-        result_printer(results)
-        recommends(results)
-        text = input("\n\nPlease try some word...\nWhat word have you tried? : ").upper()
-        correctness = int(input("and they are what likeness? : "))
-        print()
-        results = list_filter_word(results, text, correctness)
 
-def list_filter_word(results, word, number):
+        result_printer(results, "Possible answer")
+        recommends(results)
+
+        print("\n\nPlease try some word...")
+        text = input("What word have you tried? : ").upper()
+        correctness = int(input("and they are what likeness? : "))
+        screen_clear()
+        results = password_filter(results, text, correctness)
+
+def password_filter(results, word, number):
+    """Filters the password that does not satisfies the relationship"""
     possible_answer=[]
-    # print("----- Word -----|-- Similarity --")
+    # print("----- Word -----|-- Similarity --") # FOR DEBUG
     for check_answer in results:
         n = 0
         for i,v in enumerate(word):
             if check_answer[i] == v:
                 n += 1
-        # print(check_answer, n)
+        # print(check_answer, n) # FOR DEBUG
         if n == number:
             possible_answer.append(check_answer)
-    # print(possible_answer)
+    # print(possible_answer) # FOR DEBUG
     return possible_answer
 
-def result_printer(results):
-    """Designing the way that the possible answer will be print out"""
+def recommends(results):
+    """Calculate the word relationship for a chance of password elimination"""
+    length = len(results[0])
+    chars, answer = [], []
+    for i in range(length):
+        s= "".join(v[i] for v in results)
+        char = max((s.count(j),j)for j in s)
+        chars.append(char)
 
-    print("Possible answer is...")
-    print("---------------------")
+    max_char = max(chars)
+
+    for i in results:
+        if i[chars.index(max_char)] == max_char[1]:
+            answer.append(i)
+
+    result_printer(answer, "Recommended")
+
+def result_printer(results, wording):
+    """Designing the way that the possible answer will be print out"""
+    print(wording)
+    print("-" * len(wording))
     for i in results:
         print(i, end="\t")
+    print("\n")
 
+def command_center(results):
+    """Redirect additional features using commands"""
+    print("Welcome to command center. Here's what we can do...\n 1. /edit 2. /quit")
+    actions = input("What do you want to do?")
+    if actions == "/quit" or actions == "2":
+        exit()
+    elif actions == "/edit" or actions == "1":
+        result_printer(results, "What word do you want to edit?")
+
+def list_editor(results):
+    """Make the item in the list editable using this function."""
+
+def screen_clear():
+    """Cleaning screen for the program. Does not work more than this"""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def screen_size():
+    """Calculate screen size in length and width"""
+    root = tkinter.Tk()
+    root.withdraw()
+
+    width, height = root.winfo_screenwidth(), root.winfo_screenheight()
+    return width, height
+
+"""Sample case
+shot
+hurt
+sell
+give
+sure
+gear
+sent
+fire
+glow
+week
+ones
+sick
+"""
 main()
